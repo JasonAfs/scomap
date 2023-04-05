0243acde805de11b07bb1a1a8f0934594a8bac51
Jason <jason.afonso.fernandes@gmail.com>
2023-04-05 20:31:20 +0200

    Installation nextjs + debut homepage + installation tailwindcss

---
+++ components/recommendation_card.js
@@ -0,0 +1,61 @@
+import Card from './card'
+
+const Recommendation_card = () => {
+    return (
+    
+
+        <div class="flex justify-between">
+            <a class=" bg-[#fbf0dc] rounded-xl h-52 md:h-80" href="">
+
+                
+                <img src="https://a0.muscache.com/im/pictures/a433b4d0-8183-4523-b4c5-99b81c5729c1.jpg?im_w=320" class="rounded-t-xl max-h-44" />
+
+                
+                <p class="text-xl md:text-3xl  pt-5 pl-3 text-black"> Bandung </p>
+
+                
+                <p class="text-xs md:text-lg font-light text-black pt-3 pl-3 pb-10"> 117 kilometers away </p>
+
+            </a>
+            <a class=" bg-[#dbc0a5] rounded-xl h-52 md:h-80" href="">
+
+                
+                <img src="https://a0.muscache.com/im/pictures/db8167f7-5c57-4684-80ae-4350c73e45ef.jpg?im_w=320" class="rounded-t-xl max-h-44" />
+
+                
+                <p class="text-xl md:text-3xl text-gray-50 pt-5 pl-3"> Lembang </p>
+
+                
+                <p class="text-xs md:text-lg font-light text-gray-50 pt-3 pl-3 pb-10"> 109 kilometers away </p>
+
+            </a>
+            <a class=" bg-[#c2b19f] rounded-xl h-52 md:h-80" href="">
+
+                
+                <img src="https://a0.muscache.com/im/pictures/ffde0c4b-1889-4d11-bb00-41411d34fdfc.jpg?im_w=320" class="rounded-t-xl max-h-44" />
+
+                
+                <p class="text-xl md:text-3xl text-gray-50 pt-5 pl-3"> Semarang </p>
+
+                
+                <p class="text-xs md:text-lg font-light text-gray-50 pt-3 pl-3 pb-10"> 406 kilometers away </p>
+
+            </a>
+            <a class=" bg-[#352d21] rounded-xl h-52 md:h-80" href="">
+
+                
+                <img src="https://a0.muscache.com/im/pictures/03bb6d0a-5ccb-47e1-83fc-b7ad87e7f8c3.jpg?im_w=320" class="rounded-t-xl max-h-44" />
+
+                <p class="text-xl md:text-3xl text-gray-50 pt-5 pl-3"> Serang </p>
+
+                
+                <p class="text-xs md:text-lg font-light text-gray-50 pt-3 pl-3 pb-10"> 78 kilometers away </p>
+
+            </a>
+        </div> 
+
+    
+    )
+}
+
+export default Recommendation_card
